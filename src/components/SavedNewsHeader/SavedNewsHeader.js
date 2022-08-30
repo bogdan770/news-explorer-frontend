8b81d465098c8728d91e49939c29aaef0dfa1efe
Bogdan <39549904+bogdan770@users.noreply.github.com>
2022-08-30 15:51:54 +0300

    Stage_2_done_issues

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -1,12 +1,15 @@
 function SavedNewsHeader(props) {
   return (
-    <div className="saved-news__container">
-      <h4 className="saved-news__title">Saved articles</h4>
-      <h2 className="saved-news__details">
+    <div className="header__contaner_saved-news">
+      <h4 className="header__title_saved-news">Saved articles</h4>
+      <h2 className="header__details_saved-news">
         {props.userName}, you have {props.counter} saved articles
       </h2>
-      <h3 className="saved-news__keywords">
-        {"By keywords: Nature, Yellowstone, and 2 other"}
+      <h3 className="header__keywords_saved-news">
+        {"By keywords: "}
+        <span className="header__keywords_saved-news header__keywords_saved-news_bold">
+          Nature, Yellowstone and 2 other
+        </span>
       </h3>
     </div>
   );
